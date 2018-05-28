3dd3baca20efc0295a672c89101f7f9898f3f64d
Yan <yliseenko@gmail.com>
2018-05-28 20:45:22 +0300

    fix
    tasks in files

---
+++ Лаба5/laba05/cmake-build-debug/CMakeFiles/laba05.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba05.dir/laba05/Complex.cpp.o"
+  "CMakeFiles/laba05.dir/laba05/main.cpp.o"
+  "laba05.pdb"
+  "laba05"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
